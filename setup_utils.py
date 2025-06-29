8cb6abe2bbf4c071cec1ca930c1b9746c9ce277d
Proxypro2012 <kabir.tiwari@outlook.com>
2025-06-29 13:31:03 -0400

    NEW 2

--- setup_utils.py
+++ setup_utils.py
@@ -3,12 +3,9 @@ import sys
 import platform
 
 def install_package(package_name: str):
-    # Detect OS
+
     system = platform.system().lower()
 
-    # Use 'pip3' or 'pip' based on OS
-    # On Windows: usually 'pip'
-    # On Linux/macOS: usually 'pip3'
     if system == "windows":
         cmd = [sys.executable, "-m", "pip", "install", package_name]
     else:
